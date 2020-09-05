8aac5c8615e2b437b08986e3b5828c4a6e384731
hejie <565703319@qq.com>
2020-09-05 10:38:22 +0800

    [hejie]: accept rangeAndSum

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,18 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const arr = [];
+  if (start > end) {
+    for (let index = start; index > end; index -= 1) {
+      arr.push(index);
+    }
+  }
+  if (start < end) {
+    for (let index = start; index < end; index += 1) {
+      arr.push(index);
+    }
+  }
+  return arr;
 }
 
 export function sum(...numbers) {
@@ -17,6 +27,10 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let result = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    const element = numbers[index];
+    result += element;
+  }
+  return result;
 }

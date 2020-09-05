e3876c395d564092c7da1ec76a0fdf447e455d9e
hejie <565703319@qq.com>
2020-09-05 11:10:54 +0800

    [hejie]: accept arrayToList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,19 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  let node;
+  const head = { value: array[0], next: null };
+  let preNode = head;
+  for (let index = 1; index < array.length; index += 1) {
+    node = { value: array[index], next: null };
+    preNode.next = node;
+    preNode = node;
+  }
+  return head;
 }

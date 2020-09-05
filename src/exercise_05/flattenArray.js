5b9552fea13eeee9f2c252c572e420a65359ea22
hejie <565703319@qq.com>
2020-09-05 11:42:47 +0800

    [hejie]: accept flatten_array_spec

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,21 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const result = [];
+  let cnt = 0;
+  function flatten(arr) {
+    for (let index = 0; index < arr.length; index += 1) {
+      if (cnt < 2 && Array.isArray(arr[index])) {
+        cnt += 1;
+        flatten(arr[index]);
+      } else {
+        result.push(arr[index]);
+      }
+    }
+  }
+  flatten(array);
+  return result;
 }

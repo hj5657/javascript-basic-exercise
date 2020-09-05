121aaae70702173e720f42574157281123b222af
hejie <565703319@qq.com>
2020-09-05 10:22:09 +0800

    [hejie]: accept characterCounter

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,20 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let number = 0;
+  if (string === undefined || string === '' || string === null) {
+    number = 0;
+  } else if (prediction === undefined) {
+    number = string.length;
+  } else {
+    let temp = 0;
+    for (let index = 0; index < string.length; index += 1) {
+      const element = string[index];
+      if (prediction(element)) {
+        temp += 1;
+      }
+    }
+    number = temp;
+  }
+  return number;
 }
